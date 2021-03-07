36bb594a8ab6eecf5661e5f12d3f19b63691bd98
north-wanderer <castle_of_daugulas@pm.me>
2021-03-07 16:31:26 +0800

    14: Carousel-2

--- Week 14/NOTE.md
+++ Week 14/NOTE.md
@@ -1,1 +1,18 @@
-学习笔记
+# 学习笔记
+- document上监听的鼠标事件在鼠标离开浏览器的时候也能触发
+- 处理拖拽的基本结构
+```javascript
+this.root.addEventListener('mousedown', (event) => {
+  console.log('mouse-down');
+  let move = (event) => {
+    console.log('mouse-move');
+  }
+  let up = event => {
+    console.log('mouse-up');
+    document.removeEventListener('mousemove', move)
+    document.removeEventListener('mouseup', up)
+  }
+  document.addEventListener('mousemove', move)
+  document.addEventListener('mouseup', up)
+})
+```
--- Week 14/carousel/main.jsx
+++ Week 14/carousel/main.jsx
@@ -17,11 +17,43 @@ class Carousel extends Component{
   }
   render() {
     this.root = document.createElement('div')
+    this.root.classList.add('carousel')
     for (let record of this.attributes.src) {
-      let child = document.createElement('img')
-      child.src = record
+      let child = document.createElement('div')
+      child.style.backgroundImage = `url(${record})`
       this.root.appendChild(child)
     }
+    this.root.addEventListener('mousedown', (event) => {
+      console.log('mouse-down');
+      let move = (event) => {
+        console.log('mouse-move');
+      }
+      let up = event => {
+        console.log('mouse-up');
+        document.removeEventListener('mousemove', move)
+        document.removeEventListener('mouseup', up)
+      }
+      document.addEventListener('mousemove', move)
+      document.addEventListener('mouseup', up)
+    })
+    // let currentIndex = 0
+    // setInterval(() => {
+    //   let { children } = this.root
+    //   let nextIndex = (currentIndex + 1) % children.length
+
+    //   let current = children[currentIndex]
+    //   let next = children[nextIndex]
+
+    //   next.style.transition = 'none'
+    //   next.style.transform = `translateX(${100 - nextIndex * 100}%)`
+
+    //   setTimeout(() => {
+    //     next.style.transition = ''
+    //     current.style.transform = `translateX(${-100 - currentIndex * 100}%)`
+    //     next.style.transform = `translateX(${-nextIndex * 100}%)`
+    //     currentIndex = nextIndex
+    //   }, 16)
+    // }, 3000)
     return this.root
   }
   mountTo(parent) {
